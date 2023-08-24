ecdafab18b71f8ddc8fd48529d4f39e66be061d7
ade18121003@gmail.com <ade18121003@gmail.com>
2023-08-24 14:21:11 +0700

    revisi code :)

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-//function tambah buku/insert dengan object
-function tambahBuku(judul, penulis, thn_terbit, stok) {
-  //properti
+// Membuat objek buku
+function Buku(judul, penulis, thn_terbit, stok) {
   this.judul = judul;
   this.penulis = penulis;
   this.thn_terbit = thn_terbit;
   this.stok = stok;
-
-  this.judulBuku = function () {
-    return alert("Judul Buku " + this.judul);
-  };
-  this.penulisBuku = function () {
-    return alert("Penulis Buku " + this.penulis);
-  };
-  this.thnBuku = function () {
-    return alert("Tahun Terbit Buku " + this.thn_terbit);
-  };
-  this.stokBuku = function () {
-    return alert("Stok Buku " + this.stok);
-  };
 }
 
-//Variable untuk menyimpan isi prompt
-var buku = new tambahBuku(
-  prompt("Masukan Judul Buku "),
-  prompt("Masukan Penulis Buku "),
-  prompt("Masukan Tahun Terbit Buku "),
-  prompt("Masukan Stok Buku ")
-);
+// Menambahkan metode ke objek buku
+Buku.prototype.getJudul = function () {
+  return "Judul Buku: " + this.judul;
+};
 
-//panggil function
-buku.judulBuku();
-buku.penulisBuku();
-buku.thnBuku();
-buku.stokBuku();
+Buku.prototype.getPenulis = function () {
+  return "Penulis Buku: " + this.penulis;
+};
+
+Buku.prototype.getTahunTerbit = function () {
+  return "Tahun Terbit Buku: " + this.thn_terbit;
+};
+
+Buku.prototype.getStok = function () {
+  return "Stok Buku: " + this.stok;
+};
+
+// Membuat objek untuk memeriksa stok
+function CekStok(buku) {
+  this.buku = buku;
+}
+
+CekStok.prototype.getStokBuku = function () {
+  return "Stok Buku " + this.buku.judul + ": " + this.buku.stok + " buah";
+};
+
+// Menggunakan prompt untuk mengisi informasi buku
+var judul = prompt("Masukkan Judul Buku");
+var penulis = prompt("Masukkan Penulis Buku");
+var thn_terbit = prompt("Masukkan Tahun Terbit Buku");
+var stok = prompt("Masukkan Stok Buku");
+
+// Membuat objek buku baru
+var bukuBaru = new Buku(judul, penulis, thn_terbit, stok);
+
+// Membuat objek cek stok
+var stokBuku = new CekStok(bukuBaru);
+
+// Menampilkan informasi buku
+alert(bukuBaru.getJudul());
+alert(bukuBaru.getPenulis());
+alert(bukuBaru.getTahunTerbit());
+alert(bukuBaru.getStok());
+
+// Menampilkan stok buku
+alert(stokBuku.getStokBuku());
